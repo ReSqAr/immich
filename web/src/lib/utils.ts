d580a0ee155e0e694e7e7b1d71b524d522414c02
Yasin Zähringer <yasinzaehringer-github@yhjz.de>
2025-01-12 16:44:30 +0000

    web: quicker memorylane rotation

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -489,6 +489,7 @@ export async function loadMemorylanes() {
       day: '2-digit',
       hour: '2-digit',
       minute: '2-digit',
+      second: '2-digit',
       hour12: false,
     })
     .replace(',', '');
