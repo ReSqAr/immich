73002cec5d0cc8abef4573acf46a8be1c61be93f
Yasin Zähringer <yasinzaehringer-github@yhjz.de>
2025-01-12 12:59:45 +0000

    server: improved on memorylane_cluster_query.sql

--- server/src/resources/sql.ts
+++ server/src/resources/sql.ts
@@ -14,137 +14,160 @@ export const memorylaneClusterQuery = `WITH
 
     cluster_data AS (
         SELECT
-            c.cluster_id                           AS cluster_id,
-            c.cluster_start                        AS cluster_start,
-            c.cluster_end                          AS cluster_end,
+            c.cluster_id,
+            c.cluster_start,
+            c.cluster_end,
+            c."ownerId" AS owner_id,
             JSONB_BUILD_OBJECT(
                     'cities', c.cities,
                     'states', c.states,
                     'countries', c.countries
-            )                                      AS cluster_location_distribution,
-            SQRT(c.cluster_cardinality_score_ge_0) AS weight
+            )           AS cluster_location_distribution,
+            c.cluster_cardinality_score_ge_0
         FROM asset_dbscan_clusters c
              CROSS JOIN CONSTANTS co
         WHERE
               c.cluster_cardinality_score_ge_0 >= co.RESULT_LIMIT
           AND c."ownerId" = ANY (co.USER_IDS)
     ),
-    cluster_w AS (
-        SELECT
-            cd.cluster_id,
-            cd.cluster_start,
-            cd.cluster_end,
-            cd.cluster_location_distribution,
-            cd.weight,
-            SUM(cd.weight) OVER ()                       AS total_weight,
-            SUM(cd.weight) OVER (ORDER BY cd.cluster_id) AS right_cumulative
-        FROM cluster_data cd
-    ),
-    cluster_w2 AS (
-        SELECT
-            cw.cluster_id,
-            cw.cluster_start,
-            cw.cluster_end,
-            cw.cluster_location_distribution,
-            cw.weight,
-            cw.total_weight,
-            cw.right_cumulative,
-            COALESCE(
-                            LAG(cw.right_cumulative) OVER (ORDER BY cw.cluster_id),
-                            0
-            ) AS left_cumulative
-        FROM cluster_w cw
-    ),
-    chosen_cluster AS (
-        SELECT
-            cw2.cluster_id,
-            cw2.cluster_start,
-            cw2.cluster_end,
-            cw2.cluster_location_distribution
-        FROM cluster_w2 cw2
-             CROSS JOIN CONSTANTS c
-        WHERE
-            (c.SEED % ROUND(1367 * cw2.total_weight)::BIGINT) BETWEEN 1367 * cw2.left_cumulative AND 1367 * cw2.right_cumulative
-        LIMIT 1
-    ),
 
-    data AS (
+    asset_data AS (
         SELECT
             ad.id,
             ad.ts,
-            1 + ad.normalized_quality_score AS weight
+            ad.cluster_id,
+            ad."ownerId" AS owner_id,
+            ad.normalized_quality_score
         FROM asset_analysis ad
-             JOIN chosen_cluster cc ON ad.cluster_id = cc.cluster_id
+             CROSS JOIN CONSTANTS co
         WHERE
-            ad.normalized_quality_score >= 0
+              ad.normalized_quality_score >= 0
+          AND ad."ownerId" = ANY (co.USER_IDS)
     ),
-    w AS (
+
+    selected_cluster AS (
+        WITH
+            weighted_data AS (
+                SELECT
+                    d.cluster_id,
+                    d.owner_id,
+                    d.cluster_start,
+                    d.cluster_end,
+                    d.cluster_location_distribution,
+                    SQRT(d.cluster_cardinality_score_ge_0) AS weight
+                FROM cluster_data d
+                     CROSS JOIN CONSTANTS c
+                WHERE
+                    d.cluster_cardinality_score_ge_0 >= c.RESULT_LIMIT
+            ),
+
+            weighted_data_running_sum AS (
+                SELECT
+                    wd.*,
+                    SUM(wd.weight) OVER ()                       AS total_weight,
+                    SUM(wd.weight) OVER (ORDER BY wd.cluster_id) AS right_cumulative
+                FROM weighted_data wd
+            ),
+
+            weighted_data_bands AS (
+                SELECT
+                    wdrs.*,
+                    COALESCE(LAG(wdrs.right_cumulative) OVER (ORDER BY wdrs.cluster_id), 0) AS left_cumulative
+                FROM weighted_data_running_sum wdrs
+            )
+
         SELECT
-            d.id,
-            d.ts,
-            d.weight,
-            SUM(d.weight) OVER ()              AS total_weight,
-            SUM(d.weight) OVER (ORDER BY d.ts) AS right_cumulative
-        FROM data d
-    ),
-    w2 AS (
-        SELECT
-            w.id,
-            w.ts,
-            w.weight,
-            w.total_weight,
-            w.right_cumulative,
-            COALESCE(LAG(w.right_cumulative) OVER (ORDER BY w.ts), 0) AS left_cumulative
-        FROM w
-    ),
-    candidates AS (
-        SELECT
-            i AS draw_number,
-            w2.id,
-            w2.ts
-        FROM w2
-             CROSS JOIN CONSTANTS c
-             JOIN GENERATE_SERIES(0, 2 * c.RESULT_LIMIT) i
-                  ON (((c.SEED # i)::BIGINT * 73244475::BIGINT) % 4294967296::BIGINT) %
-                     ROUND(1367 * w2.total_weight)::BIGINT BETWEEN 1367 * w2.left_cumulative AND 1367 * w2.right_cumulative
-        ORDER BY draw_number
-    ),
-    candidates_with_prev AS (
-        SELECT
-            a.draw_number,
-            a.id,
-            a.ts,
-            LAG(a.ts) OVER (ORDER BY a.ts) AS prev_ts
-        FROM candidates a
-             CROSS JOIN CONSTANTS c
-    ),
-    filtered_candidates AS (
-        SELECT
-            sc.draw_number,
-            sc.id,
-            sc.ts,
-            ROW_NUMBER() OVER (ORDER BY sc.draw_number) AS row_number
-        FROM candidates_with_prev sc
+            wdb.cluster_id,
+            wdb.owner_id,
+            wdb.cluster_start,
+            wdb.cluster_end,
+            wdb.cluster_location_distribution
+        FROM weighted_data_bands wdb
              CROSS JOIN CONSTANTS c
         WHERE
-             sc.prev_ts IS NULL
-          OR sc.ts - sc.prev_ts >= c.MIN_TIME_BETWEEN_PHOTOS
-        ORDER BY sc.draw_number
+            (c.SEED % ROUND(1367 * wdb.total_weight)::BIGINT)
+                BETWEEN 1367 * wdb.left_cumulative AND 1367 * wdb.right_cumulative
+        LIMIT 1
+    ),
+
+    selected_assets AS (
+        WITH
+            weighted_data AS (
+                SELECT
+                    d.id,
+                    d.ts,
+                    1 + d.normalized_quality_score AS weight
+                FROM asset_data d
+                     JOIN selected_cluster sy USING (cluster_id, owner_id)
+            ),
+
+            weighted_data_running_sum AS (
+                SELECT
+                    wd.*,
+                    SUM(wd.weight) OVER ()               AS total_weight,
+                    SUM(wd.weight) OVER (ORDER BY wd.ts) AS right_cumulative
+                FROM weighted_data wd
+            ),
+
+            weighted_data_bands AS (
+                SELECT
+                    wdrs.*,
+                    COALESCE(LAG(wdrs.right_cumulative) OVER (ORDER BY wdrs.ts), 0) AS left_cumulative
+                FROM weighted_data_running_sum wdrs
+            ),
+
+            candidates AS (
+                SELECT
+                    i AS draw_number,
+                    wb.id,
+                    wb.ts
+                FROM weighted_data_bands wb
+                     CROSS JOIN CONSTANTS c
+                     JOIN GENERATE_SERIES(0, 2 * c.RESULT_LIMIT) i
+                          ON (((c.SEED # i)::BIGINT * 73244475::BIGINT) % 4294967296::BIGINT) % ROUND(1367 * wb.total_weight)::BIGINT
+                              BETWEEN 1367 * wb.left_cumulative AND 1367 * wb.right_cumulative
+            ),
+
+            candidates_with_lookback AS (
+                SELECT
+                    c.draw_number,
+                    c.id,
+                    c.ts,
+                    LAG(c.ts) OVER (ORDER BY c.ts, c.draw_number) AS prev_ts
+                FROM candidates c
+            ),
+
+            filtered_candidates AS (
+                SELECT
+                    ROW_NUMBER() OVER (ORDER BY cwl.draw_number) AS draw_number,
+                    cwl.id,
+                    cwl.ts
+                FROM candidates_with_lookback cwl
+                     CROSS JOIN CONSTANTS c
+                WHERE
+                     cwl.prev_ts IS NULL
+                  OR cwl.ts - cwl.prev_ts >= c.MIN_TIME_BETWEEN_PHOTOS
+            )
+
+        SELECT
+            saf.draw_number,
+            saf.id,
+            saf.ts
+        FROM filtered_candidates saf
+             CROSS JOIN CONSTANTS c
+        WHERE
+            saf.draw_number <= c.RESULT_LIMIT
     )
 
-
 SELECT
-    fc.id,
-    cc.cluster_id,
-    cc.cluster_start,
-    cc.cluster_end,
-    cc.cluster_location_distribution
-FROM filtered_candidates fc
-     CROSS JOIN CONSTANTS c
-     CROSS JOIN chosen_cluster cc
-WHERE
-    fc.row_number <= c.RESULT_LIMIT
-ORDER BY fc.ts`;
+    sa.id,
+    sc.cluster_id,
+    sc.cluster_start,
+    sc.cluster_end,
+    sc.cluster_location_distribution
+FROM selected_assets sa
+     CROSS JOIN selected_cluster sc
+ORDER BY sa.ts`;
 
 export const memorylanePersonQuery = `WITH
     CONSTANTS AS (
